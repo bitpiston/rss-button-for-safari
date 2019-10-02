05ac2c3850aa5d2bff06e8e7e3d7d4c1fbf8a2d2
Jan Pingel <jpingel@bitpiston.com>
2019-10-02 15:00:01 -0700

    Reinstate the Reeder 4 warning for now

--- RSS-Button-MacOS/SettingsManager.swift
+++ RSS-Button-MacOS/SettingsManager.swift
@@ -19,8 +19,8 @@ class SettingsManager {
     let badgeButtonKey = "badgeButtonState"
     let unsupportedHandlers = [
         "com.mentalfaculty.cream.mac",
-        "com.reederapp.rkit2.mac", // Reeder v3
-        //"com.reederapp.macos", // Reeder v4 (fixed in 4.1.5?)
+        "com.reederapp.rkit2.mac",     // Reeder v3
+        "com.reederapp.macos",         // Reeder v4 (fixed in 4.1.5?)
     ]
     
     init() {
